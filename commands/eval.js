ef0c00635eef2f1815e6607c4e5b7f763c3c69dc
newracket2 <aniket.9upt@gmail.com>
2021-03-26 23:02:06 -0700

    Updated eval command

--- commands/eval.js
+++ commands/eval.js
@@ -6,15 +6,14 @@ module.exports = {
   aliases: ["e"],
   execute(message, args, client) {
     if (message.author.id == "301200493307494400") {
-      console.log("Args: ", args.join(" "));
       let output = eval(args.join(" "));
-      console.log("Output: ", output);
       if (output == undefined) {
         return message.channel.send("Output is undefined");
       }
       if (typeof output != "string") {
         output = output.toString();
       }
+      output = output.split("\r\n").join("");
       const outputTexts = output.match(/.{1,1900}/g);
 
       outputTexts.forEach(outputText => {
