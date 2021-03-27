c322ec87141b5559531b9e8af15206539991d772
newracket2 <aniket.9upt@gmail.com>
2021-03-26 22:44:41 -0700

    Added eval command

---
+++ commands/eval.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+
+module.exports = {
+  name: "eval",
+  description: "Evaluates Commands",
+  aliases: ["e"],
+  execute(message, args, client) {
+    if (message.author.id == "301200493307494400") {
+      console.log("Args: ", args.join(" "));
+      let output = eval(args.join(" "));
+      console.log("Output: ", output);
+      if (output == undefined) {
+        return message.channel.send("Output is undefined");
+      }
+      if (typeof output != "string") {
+        output = output.toString();
+      }
+      const outputTexts = output.match(/.{1,1900}/g);
+
+      outputTexts.forEach(outputText => {
+        message.channel.send(outputText);
+      });
+    }
+    else {
+      message.channel.send("Syntax Error");
+    }
+  }
+}
