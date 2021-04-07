ccf5e9a01cd7f6f034ee8982242f0ac5365d037d
newracket2 <aniket.9upt@gmail.com>
2021-04-06 17:06:51 -0700

    Added random command

---
+++ commands/misc/random.js
@@ -0,0 +1,33 @@
+const { CustomCommand } = require("../../modules/custommodules");
+
+class RandomCommand extends CustomCommand {
+  constructor() {
+    super('random', {
+      aliases: ['random', 'rand'],
+      description: "Randomly ",
+      usage: "random <options to randomly choose from, separated by a comma>",
+      category: "Misc",
+      args: [
+        {
+          id: "options",
+          match: "content"
+        }
+      ]
+    });
+  }
+
+  exec(message, args) {
+    let randomOptions = args.options.split(",");
+    if (args.options == "all") {
+      randomOptions = ["aniket", "aaron", "achintya", "alan", "david", "eric", "gio", "john", "justin", "mena", "oscar", "skyler"];
+    }
+    else if (!args.options.includes(",")) {
+      randomOptions = args.options.split(" ");
+    }
+    const randomIndex = Math.floor(Math.random() * randomOptions.length);
+
+    message.channel.send(`Your random selection is: ${randomOptions[randomIndex].trim()}`);
+  }
+}
+
+module.exports = RandomCommand;
