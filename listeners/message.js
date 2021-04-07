ec4e78bca95cf34049effc1670a15e9c7036aa96
newracket2 <aniket.9upt@gmail.com>
2021-04-07 12:06:24 -0700

    Added deletion of message if it wasn't an appeal message in #appeals

--- listeners/message.js
+++ listeners/message.js
@@ -17,6 +17,9 @@ class MessageListener extends Listener {
         return message.channel.send("Error: " + error);
       }
     }
+    else if (message.channel.id == "828155824966205440" && message.author != this.client.user && !["appeal", "approve", "accept", "compromise", "comp", "deny", "reject"].includes(message.content.split(" ")[0].slice(1))) {
+      message.delete().then(() => message.author.send(`Your message was deleted due to it not being an appeal command. Your message was: ${message.content}`));
+    }
   }
 }
 
