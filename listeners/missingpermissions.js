e541625112876146a6125d5bd6a033e2a858f22b
newracket <aniket.9upt@gmail.com>
2021-07-16 10:44:41 -0700

    Fixed bug with deleting appeal messages

--- listeners/missingpermissions.js
+++ listeners/missingpermissions.js
@@ -10,7 +10,16 @@ class MissingPermissionsListener extends Listener {
 
   exec(message, command, reason, missing) {
     switch (reason) {
-      case "role": return message.reply(`You are missing permissions. Roles required: ${missing.join(", ")}`);
+      case "role": {
+        if (message.channel.id == "828155824966205440") {
+          message.author.send(`You are missing permissions. Roles required: ${missing.join(", ")}`);
+          message.delete();
+        }
+        else {
+          message.reply(`You are missing permissions. Roles required: ${missing.join(", ")}`);
+        }
+        break;
+      };
       case "client": return message.reply("This bot is missing permissions to do that");
       case "user": return message.reply(`You do not have permissions to do that. Permissions required: ${missing.join(", ")}`);
     }
